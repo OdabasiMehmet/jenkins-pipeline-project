59e38b975a6d61133f9390b8825ba2549738c54c
Mehmet Odabaşı <95535935+OdabasiMehmet@users.noreply.github.com>
2022-09-15 21:52:10 +0300

    Updated from github

--- pipeline.py
+++ pipeline.py
@@ -1,17 +1,11 @@
 pipeline {
     agent any
     stages {
-        stage('build') {
-            steps {
-                echo "Clarusway_Way to Reinvent Yourself"
-                sh 'echo using shell within Jenkinsfile'
-                echo 'not using shell in the Jenkinsfile'
-            }
-        }
-        stage('Jenkinsfile') {
+        stage('run') {
             steps {
                 echo 'Clarusway_Way to Reinvent Yourself'
-                sh 'echo Integrating Jenkins Pipeline with GitHub Webhook using Jenkinsfile'
+                sh 'python --version'
+                sh 'python pipeline.py'
             }
         }
     }
